2d43e1bc697938376cb36054e218460fb2fe3a3d
Ashley Schauer <aelschauer@gmail.com>
2021-08-03 16:24:10 -0600

    add validation for email in login

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,8 @@ const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
   const { email } = req.query;
+  if (!email) res.status(400).send({ error: "Login credentials missing or invalid" });
+
   client
     .query(`SELECT id, name, email FROM users WHERE email = '${email}';`)
     .then(({ rows: [record = {}] = [] }) => {
